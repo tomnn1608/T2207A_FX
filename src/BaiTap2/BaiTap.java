aeb5a18adbe157e3a0446bb35fa76212e2e71080
tomnn1608 <nxt16082004@gmail.com>
2023-02-10 17:14:50 +0700

    btvn

---
+++ src/BaiTap2/BaiTap.java
@@ -0,0 +1,105 @@
+package BaiTap2;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.Button;
+
+public class BaiTap implements Initializable {
+
+    public TextField Name;
+    public TextField Telephone;
+
+    public static TextField stName;
+    public static TextField stPhone;
+
+    public ObservableList<Danhba> listDanhba = FXCollections.observableArrayList();
+
+
+    public ListView<Danhba> ld;
+    public static Danhba editStudent;
+
+    public boolean ascending=true;
+
+    public TableView<Danhba> tbview;
+    public TableColumn<Danhba,String> name;
+    public TableColumn<Danhba,String> telephone;
+    public TableColumn<Danhba,Button>action;
+
+    public void submit(ActionEvent actionEvent) {
+        String n = Name.getText();
+        String t = Telephone.getText();
+
+        if (editStudent == null) {
+            Danhba d = new Danhba(n, t);
+            listDanhba.add(d);
+
+        } else {
+            for (Danhba d : listDanhba) {
+                if (d.telephone.equals(editStudent.telephone)
+                        && d.name.equals(editStudent.name)) {
+                    d.setName(n);
+                    d.setTelephone(t);
+                }
+            }
+        }
+        tbview.setItems(listDanhba);
+        tbview.refresh();
+        editStudent = null;
+        clearInput();
+    }
+
+    void clearInput() {
+        Name.clear();
+        Telephone.clear();
+    }
+
+    public void edit(MouseEvent mouseEvent) {
+        editStudent = ld.getSelectionModel().getSelectedItem();
+        Name.setText(editStudent.getName());
+        Telephone.setText(editStudent.getTelephone());
+    }
+
+    public void sort(MouseEvent mouseEvent) {
+        if (ascending) {
+            Collections.sort(listDanhba, new Comparator<Danhba>() {
+                @Override
+                public int compare(Danhba o1, Danhba o2) {
+                    return o1.name.compareTo(o2.name);
+                }
+            });
+        } else {
+            Collections.sort(listDanhba, new Comparator<Danhba>() {
+                @Override
+                public int compare(Danhba o1, Danhba o2) {
+                    return o2.name.compareTo(o1.name);
+                }
+            });
+        }
+        ascending = !ascending;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        telephone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        action.setCellValueFactory(new PropertyValueFactory<>("edit"));
+
+        stName = Name;
+        stPhone = Telephone;
+    }
+}
---
+++ src/BaiTap2/Danhba.java
@@ -0,0 +1,48 @@
+package BaiTap2;
+
+import javafx.scene.control.Button;
+
+public class Danhba {
+    String name;
+    String telephone;
+    Button edit;
+
+    public Danhba(String name, String telephone) {
+        this.name = name;
+        this.telephone = telephone;
+        this.edit = new Button("Edit");
+        this.edit.setOnAction((event) ->{
+            BaiTap.editStudent = this;
+            BaiTap.stName.setText(this.name);
+            BaiTap.stPhone.setText(this.telephone);
+        });
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String toString(){
+        return this.name+ "--"+this.telephone;
+    }
+
+    public Button getEdit() {
+        return edit;
+    }
+
+    public void setEdit(Button edit) {
+        this.edit = edit;
+    }
+}
